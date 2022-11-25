f8fdb999878d11ff5a9a7077581fffd4ae2d6c1d
NimaAbbasi <nimaabbasi228@gmail.com>
2022-11-25 13:07:46 +0330

    stylesFixed

--- component/survey/index.tsx
+++ component/survey/index.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useState } from "react";
 import { AiTwotoneLike, AiTwotoneDislike } from "react-icons/ai";
+import { useEffect } from "react";
 import styles from "./Survey.module.scss";
 
 interface Props {
@@ -12,15 +13,24 @@ const Survey = (detailId: Props) => {
   const [likeCount, setLikeCount] = useState(0);
   const [dislikeCount, setDislikeCount] = useState(0);
   const [activeBtn, setActiveBtn] = useState("none");
-  // const user = JSON.parse(localStorage.getItem("user") || "{}");
-  // const userID = user?.student.id;
+  const [userId, setUserId] = useState();
+
   const detail = detailId;
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (localStorage.getItem("user") !== null) {
+        setUserId(
+          JSON.parse(localStorage.getItem("user") || "{}")?.student._id
+        );
+      }
+    }
+  });
 
   const handleLikeClick = () => {
     axios
       .post("http://querateam1.herokuapp.com/api/course/like", {
         courseId: detail.detailId,
-        userId: "628284559a4090194c4c94bb",
+        userId: userId,
       })
       .then((response) => {
         console.log(response.data);
@@ -49,7 +59,7 @@ const Survey = (detailId: Props) => {
     axios
       .post("http://querateam1.herokuapp.com/api/course/dislike", {
         courseId: detail.detailId,
-        userId: "628284559a4090194c4c94bb",
+        userId: userId,
       })
       .then((response) => {
         console.log(response.data);
@@ -94,10 +104,10 @@ const Survey = (detailId: Props) => {
           Dislike
         </button>
       </div> */}
-      <div className="">
+      <div className={styles.btn_container}>
         <button
-          className={`btn_like_deslike ${
-            activeBtn === "like" ? "like_active" : ""
+          className={`${styles.btn_like_deslike} ${
+            activeBtn === "like" ? `${styles.like_active}` : ""
           }`}
           onClick={handleLikeClick}
         >
@@ -109,8 +119,8 @@ const Survey = (detailId: Props) => {
         </button>
 
         <button
-          className={`btn_like_deslike ${
-            activeBtn === "dislike" ? "dislike_active" : ""
+          className={`${styles.btn_like_deslike} ${
+            activeBtn === "dislike" ? `${styles.dislike_active}` : ""
           }`}
           onClick={handleDisikeClick}
         >
